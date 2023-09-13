0a3241edde50a1b10f90b893e2c6c05b0afff448
kartali mohamed <mohameekartali@gmail.com>
2023-09-13 14:16:18 +0100

    onTap function for home card

--- lib/home/widgets/custom_homecardfolder.dart
+++ lib/home/widgets/custom_homecardfolder.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class HomeCardFolder extends StatelessWidget {
   final String title;
+  final void Function()? onTap;
   final void Function()? onLongPress;
-  const HomeCardFolder({Key? key, required this.title, this.onLongPress})
+  const HomeCardFolder(
+      {Key? key, required this.title, this.onLongPress, this.onTap})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: onTap,
       onLongPress: onLongPress,
       child: Card(
         child: Column(
